5621971e506402912e0e73a5cd7d8eaef9a53368
Eric M <itsjusteza@gmail.com>
2021-12-17 11:42:40 +1000

    Add word frequency chart

---
+++ src/charts/word-frequency.tsx
@@ -0,0 +1,60 @@
+import c3 from "c3";
+import { useEffect } from "react";
+import { useChartId } from "../hooks/use-chart-id";
+import { useConversation } from "../hooks/use-conversation";
+
+export function WordFrequency({
+	lengthMin = 1,
+	lengthMax = 10,
+}: {
+	lengthMin: number;
+	lengthMax: number;
+}) {
+	const chartId = useChartId();
+	const { conversationData } = useConversation();
+
+	useEffect(() => {
+		if (!conversationData) {
+			return;
+		}
+
+		const wordCounts = conversationData.messages.reduce<{
+			[word: string]: { [participant: string]: number; total: number };
+		}>((accum, msg) => {
+			msg.words.forEach((word) => {
+				if (word.length >= lengthMin && word.length <= lengthMax) {
+					accum[word] = accum[word] ?? { total: 0 }; // Create new entry if word does not exist
+					accum[word][msg.sender] = accum[word][msg.sender] ?? 0; // Create new entry if word does not exist for sender
+					accum[word][msg.sender] = (accum[word][msg.sender] ?? 0) + 1; // Increment usage of word for sender
+					accum[word].total += 1;
+				}
+			});
+
+			return accum;
+		}, {});
+
+		let topWords = Object.keys(wordCounts)
+			.sort((a, b) => wordCounts[b].total - wordCounts[a].total)
+			.slice(0, 20);
+
+		const wordCountColumns = conversationData.participants.map<
+			[string, ...number[]]
+		>((p) => [p, ...topWords.map((w) => wordCounts[w][p])]);
+
+		c3.generate({
+			bindto: `#${chartId.current}`,
+			data: {
+				type: "bar",
+				columns: wordCountColumns,
+			},
+			axis: {
+				x: {
+					type: "category",
+					categories: topWords,
+				},
+			},
+		});
+	}, [conversationData, chartId, lengthMin, lengthMax]);
+
+	return <div id={chartId.current}></div>;
+}
--- src/subapp.tsx
+++ src/subapp.tsx
@@ -5,6 +5,7 @@ import { MessagesTimeOfDay } from "./charts/messages-time-of-day";
 import { MessagesTimeline } from "./charts/messages-timeline";
 import { ParticipantMessageDistribution } from "./charts/participant-message-distribution";
 import { ParticipantWordsDistribution } from "./charts/participant-words-distribution";
+import { WordFrequency } from "./charts/word-frequency";
 import { useConversation } from "./hooks/use-conversation";
 
 export function SubApp() {
@@ -12,6 +13,8 @@ export function SubApp() {
 	const { conversation, analyseConversationFromFiles } = useConversation();
 
 	const [lengthLimit, setLengthLimit] = useState(12);
+	const [wordLenMin, setWordLenMin] = useState(1);
+	const [wordLenMax, setWordLenMax] = useState(10);
 
 	function onFilesSelected() {
 		if (input.current && input.current.files) {
@@ -38,12 +41,28 @@ export function SubApp() {
 				</ul>
 			)}
 
-			<input
-				type="number"
-				value={lengthLimit}
-				onChange={(e) => setLengthLimit(parseInt(e.target.value))}
-			/>
+			<div>
+				Msg Length Limit
+				<input
+					type="number"
+					value={lengthLimit}
+					onChange={(e) => setLengthLimit(parseInt(e.target.value))}
+				/>
+				Word Len Min
+				<input
+					type="number"
+					value={wordLenMin}
+					onChange={(e) => setWordLenMin(parseInt(e.target.value))}
+				/>
+				Word Len Max
+				<input
+					type="number"
+					value={wordLenMax}
+					onChange={(e) => setWordLenMax(parseInt(e.target.value))}
+				/>
+			</div>
 
+			<WordFrequency lengthMin={wordLenMin} lengthMax={wordLenMax} />
 			<MessageLengths lengthLimit={lengthLimit} />
 			<MessagesTimeOfDay groupBy="Hours" />
 			<MessagesTimeOfDay groupBy="Minutes" />
